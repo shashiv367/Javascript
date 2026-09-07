cd284e13801623e2f57650f4c04e7191c98dc31f
Shashi Vardhan <vardhans367@gmail.com>
2026-09-07 17:15:14 +0530

    object object properties

---
+++ object.js
@@ -0,0 +1,16 @@
+let inp = 'name'
+
+//object
+let obj = {
+    //properties
+    name:"kkk",
+    age:23,
+    gender:"Male",
+    'work exp':4
+} 
+console.log(obj.name)//dot operator
+console.log(obj)
+console.log(typeof(obj))
+console.log(obj['work exp'])//square operator
+console.log(obj.inp)//can't use dot operator
+console.log(obj[inp])
