5f9b53977356009fd0e9d0878497b585bacb2ac6
Shashi Vardhan <vardhans367@gmail.com>
2026-09-07 17:45:55 +0530

    for in loop

--- object.js
+++ object.js
@@ -6,11 +6,28 @@ let obj = {
     name:"kkk",
     age:23,
     gender:"Male",
-    'work exp':4
+    'work exp':4,
+    laptop:{
+        cpu:17,
+        ram:16,
+        brand:'Asus'
+    }
 } 
+
 console.log(obj.name)//dot operator
 console.log(obj)
 console.log(typeof(obj))
 console.log(obj['work exp'])//square operator
 console.log(obj.inp)//can't use dot operator
 console.log(obj[inp])
+
+console.log(obj.laptop.brandl?.length)
+// delete obj.laptop
+// console.log(obj)
+
+//for in loop
+for(let key in obj){
+    //console.log(key, obj.name) not suitable here
+    console.log(key, obj[key])
+}
+
