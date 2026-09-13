953014851f1857604aa09e704003dd7f9b362d28
Shashi Vardhan <vardhans367@gmail.com>
2026-09-13 20:49:32 +0530

    arrow function

--- functions1.js
+++ functions1.js
@@ -7,11 +7,11 @@ function createStudent(name, age, course) {
     };
 }
 
-let student1 = createStudent("Hanu", 20, "CSE");
-let student2 = createStudent("Ravi", 21, "ECE");
+let student11 = createStudent("Hanu", 20, "CSE");
+let student22 = createStudent("Ravi", 21, "ECE");
 
-console.log(student1);
-console.log(student2);
+console.log(student11);
+console.log(student22);
 
 //construction function 
 function Student(name, age, course) {
@@ -25,3 +25,30 @@ let student2 = new Student("Ravi", 21, "ECE");
 
 console.log(student1);
 console.log(student2);
+
+function greet() {
+    return "hello"
+}
+//arrow function
+const greet1 = () => "hello"
+
+function double(x){
+    return x *2
+}
+
+const double = x => x*2
+
+function add(a,b){
+    return a+b
+}
+
+const add = (a,b) => a+b
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const num = [1,2,3,4,6,8,9,10]
+const evens = num.filter(n => n%2 === 0)
+
+console.log(evens)
