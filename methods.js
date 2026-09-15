01efbbb5b667f98e5b6bc43a1cb484977c2b6c35
Shashi <vardhans367@gmail.com>
2026-09-15 12:14:28 +0530

    methods intro

---
+++ methods.js
@@ -0,0 +1,12 @@
+let laptop = {
+    brand :"Dell",
+    price : 45000,
+    ram : "16gb",
+    storage : "512gb",
+
+    getlaptop : function(){
+        console.log("Hii");
+        return "hello World"
+    }
+}
+laptop.getlaptop()
