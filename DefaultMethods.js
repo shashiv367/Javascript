90593c92f826f6944a770591f0afa15ac9679a0c
shashiv367 <vardhans367@gmail.com>
2026-09-17 16:54:07 +0530

    Default methods, toUppercase,lowercase, includes, startswith etc

---
+++ DefaultMethods.js
@@ -0,0 +1,27 @@
+// toUpperCase
+let a = 'nnn'
+console.log(a.toUpperCase());
+
+//toLowerCase
+let b = 'AbDNS'
+console.log(b.toLowerCase());
+ 
+//includes
+let c = "i have a car"
+console.log(c.includes("have"));
+
+//startsWith
+let d = 'Green color'
+console.log(d.startsWith("Green"));
+
+//endswith
+let e = 'Blue color'
+console.log(e.endsWith("color"));
+
+//slice
+let f = "ooo myy gwadddd"
+console.log(f.slice(0,9));
+
+//toString
+let num = 23
+console.log(num.toString());
