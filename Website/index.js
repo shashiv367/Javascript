8fa899afe67e904844b5d4b2ec38503c328469da
Shashi Vardhan <vardhans367@gmail.com>
2026-09-18 21:19:25 +0530

    structure

---
+++ Website/index.js
@@ -0,0 +1,50 @@
+/* console.log("Hello World")
+console.log("Whatever")
+
+window.alert('this is an alert');
+window.alert('This cannot be');
+*/
+
+// document.getElementById("h1").textContent = "Hello";
+// document.getElementById("p").textContent = "Loreum ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+
+// variables assigning
+// let y;
+// let x;
+
+// x = 100;
+// console.log(x);
+
+// data types
+
+// let age = 20;
+// let price = 10.99;
+// let name = "Shashi";
+// let gpa = 2.1;
+
+// console.log(age);
+// console.log(price);
+// console.log(name);
+// console.log(gpa);
+
+// console.log(typeof age);
+// console.log(typeof price);
+// console.log(typeof name);
+// console.log(typeof gpa);
+
+// console.log(`You are ${age} with gpa of ${gpa}`);
+// console.log(`You'r name is ${name} `); 
+
+// let online = true;
+// console.log(typeof online);
+
+// let fname = "Bbb";
+// let age = 24;
+// let student = false;
+
+// document.getElementById("p1").textContent = `You're name is ${fname}`;
+// document.getElementById("p2").textContent = `You're age is ${age}`;
+// document.getElementById("p3").textContent = `Are you a student ${student}`;
+
+
